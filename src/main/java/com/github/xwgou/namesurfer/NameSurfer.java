5696b577d7f739559a4d745542b58e6030f3f2ad
Christian Stussak <stussak@mfo.de>
2015-12-09 14:58:28 +0100

    wiped out old Swing UI

--- src/main/java/com/github/xwgou/namesurfer/NameSurfer.java
+++ src/main/java/com/github/xwgou/namesurfer/NameSurfer.java
@@ -1,19 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.github.xwgou.namesurfer;
 
-import com.github.xwgou.namesurfer.gui.JSurferPanel;
 import com.github.xwgou.namesurfer.fxui.JFXNameSurfer;
 
-/**
- *
- * @author stussak
- */
 public class NameSurfer
 {
-    //public static void main( String[] args ) { JSurferPanel.main( args ); }
     public static void main( String[] args ) { JFXNameSurfer.main( args ); }
 }
